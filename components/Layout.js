aec2a5392a20ca54f2aeae5b1e6108de67b4d3fa
Ashik044 <rezaashik044@gmail.com>
2022-09-22 20:17:50 +0600

    ok

--- components/Layout.js
+++ components/Layout.js
@@ -2,13 +2,13 @@ import Nav from "./Nav";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div className="">
       <Nav />
 
-      <main className="w-full py-4 px-0">
+      <main className="w-full py-12 px-0">
         <div className="w-[90%] m-auto">{children}</div>
       </main>
-    </>
+    </div>
   );
 };
 
--- components/Nav.js
+++ components/Nav.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import logo from "../public/logo-bg.png";
 
 import Link from "next/link";
 
 function nav() {
+  const [isDark, setIsDark] = useState(true);
+
+  const setColor = () => {
+    const htmlClassName = localStorage.getItem("ColorTheme");
+    const html = document.documentElement;
+    if (htmlClassName) html.className = htmlClassName;
+  };
+  const toggleTheme = () => {
+    setIsDark(!isDark);
+    if (isDark) {
+      localStorage.removeItem("ColorTheme");
+      localStorage.setItem("ColorTheme", "dark");
+      setColor();
+    } else {
+      localStorage.removeItem("ColorTheme");
+      localStorage.setItem("ColorTheme", "ligth");
+      setColor();
+    }
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("ColorTheme") === "dark") {
+      setIsDark(false);
+      setColor();
+    } else {
+      setIsDark(true);
+      setColor();
+    }
+  }, []);
+
   return (
-    <nav className="w-full flex justify-between items-cente pt-2 pb-2 px-10 mb-8 shadow-md">
+    <nav className="w-full flex justify-between items-cente pt-2 pb-2 px-10 shadow-md dark:bg-slate-700 dark:text-white">
       <div className="flex">
         <Link href="/">
           <a className="flex justify-between items-center ">
@@ -27,12 +57,33 @@ function nav() {
         </span>
 
         <Link href="/Signup">
-          <a className="pl-2 mb-1 text-blue-600 font-bold">Signup</a>
+          <a className="pl-2 mb-1 text-blue-600 font-bold dark:text-blue-400">
+            Signup
+          </a>
         </Link>
 
         {/* <span className="material-icons-outlined" title="Logout"> logout </span>  */}
+
+        <BtnToggler onEvent={toggleTheme}>
+          {isDark ? (
+            <span class="material-icons-outlined">dark_mode</span>
+          ) : (
+            <span class="material-icons-outlined">light_mode</span>
+          )}
+        </BtnToggler>
       </div>
     </nav>
+  );
+}
+
+function BtnToggler({ children, classes = "", onEvent }) {
+  return (
+    <div
+      onClick={onEvent}
+      className={`grid place-items-center w-10 h-10 sm:w-12 sm:h-12 text-sm sm:text-base lg:text-lg rounded-full cursor-pointer text-gray-800 dark:text-gray-100 select-none ml-10 ${classes}`}
+    >
+      {children}
+    </div>
   );
 }
 
