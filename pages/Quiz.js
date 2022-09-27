c4091c3869eaba4589bf783090f31de61e7b3037
Ashik044 <rezaashik044@gmail.com>
2022-09-27 22:29:21 +0600

    done

--- pages/Quiz.js
+++ pages/Quiz.js
@@ -4,10 +4,34 @@ import ProgessBar from "../components/quiz component/ProgressBar";
 import useQuestions from "../hook/useQuestions";
 import Layout from "../components/Layout";
 import Head from "next/head";
-import { useState } from "react";
+import { useState, useReducer, useEffect } from "react";
 import { useRouter } from "next/router";
-
 import { withProtected } from "../components/route";
+import _ from "lodash";
+
+const initialState = null;
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "questions":
+      action.value.forEach((question) => {
+        question.options.forEach((option) => {
+          option.checked = false;
+        });
+      });
+      return action.value;
+
+    case "answer":
+      const questions = _.cloneDeep(state);
+      questions[action.questionID].options[action.optionIndex].checked =
+        action.value;
+
+      return questions;
+      
+    default:
+      return state;
+  }
+};
 
 const Quiz = () => {
   var router = useRouter();
@@ -15,6 +39,15 @@ const Quiz = () => {
 
   const { loading, error, questions } = useQuestions(id);
   const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const [qna, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({
+      type: "questions",
+      value: questions,
+    });
+  }, [questions]);
 
   return (
     <>
@@ -26,7 +59,7 @@ const Quiz = () => {
         <div className="mx-10 ">
           <div className="text-center mb-4">
             <h1 className="text-4xl font-bold mt-4">
-              Pick three of your favorite Star Wars Flims
+              {qna[currentQuestion].title}
             </h1>
             <h4 className="text-xl mt-2 font-semibold text-gray-600 dark:text-white">
               Question can have multiple answers
@@ -46,4 +79,3 @@ const Quiz = () => {
 };
 
 export default withProtected(Quiz);
- 
