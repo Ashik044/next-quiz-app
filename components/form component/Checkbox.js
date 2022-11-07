b974b6e2a84f872058d47a0ca0f8836e4ce887df
Ashik044 <rezaashik044@gmail.com>
2022-11-07 20:39:39 +0600

    ok

--- components/form component/Checkbox.js
+++
@@ -1,7 +0,0 @@
-export default function Checkbox({ style, text, ...rest }) {
-  return (
-    <label className={style}>
-      <input className="mr-2" type="checkbox" {...rest} /> <span>{text}</span>
-    </label>
-  );
-}
